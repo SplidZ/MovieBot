1e61315fbae264ef5d139cd31695b6b5d63ceeb5
SplidZ <86190727+SplidZ@users.noreply.github.com>
2024-05-14 13:23:16 +0200

    Create error.js

---
+++ Events/error.js
@@ -0,0 +1,16 @@
+const client = require("../index");
+
+client.on("error", (err) => {
+    console.log("Discord API Error :", err);
+});
+
+process.on("warning", (warn) => {
+    console.log("Warning :", warn);
+});
+
+process.on("unhandledRejection", (reason, p) => {
+    console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+    console.log(err, origin);
+});
